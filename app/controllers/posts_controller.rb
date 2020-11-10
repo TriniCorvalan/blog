69ad05f97f6af04553d3d25a12d14c05f1c22119
Trini <trini.4595@gmail.com>
2020-11-10 16:13:47 -0300

    index controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   http_basic_authenticate_with name: "desafiovamoscontodo", password: "XAHTJEAS23123%23", only: :dashboard
 
   def index
-    @posts = Post.where('content LIKE ?', "%#{params[:q]}%").order(id: :desc)
+    # @posts = Post.where('content LIKE ?', "%#{params[:q]}%").order(id: :desc)
   end
 
   def dashboard
