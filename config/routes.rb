ce45a2b34a6a27050973c0b90227205d34e45532
Trini <trini.4595@gmail.com>
2020-11-10 14:33:28 -0300

    cambiar sqlite por pg

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get 'posts/dashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # root 'posts#index'
+  root 'posts#index'
 end
