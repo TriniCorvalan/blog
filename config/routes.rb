2cf57295b9de704444676ac9748e2ef9dba28802
Trini <trini.4595@gmail.com>
2020-11-10 16:26:35 -0300

    root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root 'post#index'
 
   get 'posts/index'
   post 'posts/create'
   get 'posts/dashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 
 end
